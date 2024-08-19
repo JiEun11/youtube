cbd10cbcd369113718dd2f865e3c2af221cef446
Bella <shdudtnr3939@naver.com>
2024-08-20 00:24:13 +0900

    feat: 비디오 카드 만들기 - useQuery를 이용한 Mock data 사용

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,11 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import VideoCard from '../components/VideoCard';
 
 function Videos() {
   const { keyword } = useParams();
+  const { isLoading, error, data: videos } = useQuery({
+    queryKey: ['videos', keyword],
+    queryFn: () => 
+      fetch(`/videos/${keyword? 'search' : 'popular'}.json`)
+      .then((res) => res.json())
+      .then((data) => data.items)
+  });
 
   return (
-    <div>Videos {keyword ? `${keyword}` : `hot`} </div>
+    <>
+      <div>Videos {keyword ? `${keyword}` : `hot`} </div>
+      {isLoading && <p>Loading....</p>}
+      {error && <p>Something is wrong</p>}
+      {videos && <ul>
+        { videos.map(video => <VideoCard key={video.id} video={video}/>)}
+        </ul>}
+    </>
   )
 }
 
