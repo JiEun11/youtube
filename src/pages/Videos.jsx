2732833e36157c4fe4e16e734dcbde4d67b2bf77
bella <shdudtnr3939@naver.com>
2024-08-25 17:29:01 +0900

    fix: mock 데이터 이용해서 비디오 카드 component list로 보여주기

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import VideoCard from './VideoCard';
 
 function Videos() {
   const { keyword } = useParams();
-
+  const {isLoading, error, data: videos} = useQuery({
+    queryKey: ["videos", keyword],
+    queryFn: () =>
+      fetch(`/videos/${keyword ? "search" : "popular"}.json`)
+        .then((res) => res.json())
+        .then((data) => data.items),
+  });
   return (
-    <div>Videos {keyword ? `${keyword}` : `hot`} </div>
+    <>
+      <div>Videos {keyword ? `${keyword}` : `hot`} </div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong</p>}
+      {videos && <ul>
+        {videos.map(video => <VideoCard key={video.id} video={video} />)}
+        </ul>
+      }
+    </>
   )
 }
 
