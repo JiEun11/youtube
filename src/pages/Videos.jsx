1a4c6216d4feeae33a9130d22869e211429d1d44
bella <shdudtnr3939@naver.com>
2024-08-25 18:23:40 +0900

    feat: error 페이지 추가

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from './VideoCard';
 import axios from 'axios';
+import ErrorPage from './ErrorPage';
 
 function Videos() {
   const { keyword } = useParams();
@@ -16,7 +17,7 @@ function Videos() {
     <>
       <div>Videos {keyword ? `${keyword}` : `hot`} </div>
       {isLoading && <p>Loading...</p>}
-      {error && <p>Something is wrong</p>}
+      {error && <ErrorPage />}
       {videos && <ul>
         {videos.map(video => <VideoCard key={video.id} video={video} />)}
         </ul>
