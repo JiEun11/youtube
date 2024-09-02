e76814566813f9619ea5cf4cc338ffd422dba886
Bella <shdudtnr3939@naver.com>
2024-09-02 23:28:15 +0900

    feat: Client 버전 2가지로 분리 - DI 적용

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -3,13 +3,11 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from './VideoCard';
 import ErrorPage from './ErrorPage';
-import FakeYoutube from '../api/fakeYoutube';
-import Youtube from '../api/youtube';
 import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 function Videos() {
   const { keyword } = useParams();
-  const {youtube} = useYoutubeApi();
+  const { youtube } = useYoutubeApi();
   const {isLoading, error, data: videos} = useQuery({
     queryKey: ["videos", keyword],
     queryFn: () => {
